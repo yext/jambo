a876812b03787e6f6a4371144f0d18bd28ad5712
Matt Bowman <mbowman@yext.com>
2020-02-20 13:49:25 -0500

    Clean up

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 let fs = require('file-system');
-import { snakeCase } from "change-case";
+let { snakeCase } = require('change-case');
 let hbs = require('handlebars');
 
+// BUILD
+
 // Read in the root config
-let rootConfig = JSON.parse(fs.readFile('config.json'));
+let rootConfig = JSON.parse(fs.readFileSync('config.json'));
 
 // Read in the page-specific configs
-let pagesConfig = fs.recurse('config')
-	.reduce((acc, cur) => {
-		let id = snakeCase(cur);
-		acc[id] = JSON.parse(fs.readFile(cur));
-		return acc;
-	});
 
+let pagesConfig = {};
+
+fs.recurseSync('config', (path, relative, filename) => {
+	let id = snakeCase(relative);
+	pagesConfig[id] = JSON.parse(fs.readFileSync(path));
+})
 
 // Import theme partials if necessary
-let themePartials = rootConfig.getTheme() ? fs.recurse('themes/' + rootConfig.getTheme()) : [];
-themePartials.forEach((x) => {
-	hbs.registerPartial(snakeCase(x), fs.readFile(x));
-});
+if (rootConfig.getTheme()) {
+	fs.recurseSync('themes/' + rootConfig.getTheme(), (path, relative, filename) => {
+		hbs.registerPartial(snakeCase(relative), fs.readFileSync(path));
+	});
+}
 
 // Import partials from repository
-fs.recurse('partials').forEach((x) => {
-	hbs.registerPartial(snakeCase(x), fs.readFile(x));
+// TODO: Read from root config a list of all directories containing partials
+fs.recurseSync('partials', (path, relative, filename) => {
+	hbs.registerPartial(snakeCase(relative), fs.readFileSync(path));
 });
 
+// END BUILD
