7f523b7f4c3dfdbacce630540a9e50668e83a33c
Tom Meyer <tmeyer@yext.com>
2020-08-05 15:41:12 -0400

    Add the translatePlural helper to Jambo. (#64)
    This PR adds a translatePlural HBS helper to Jambo. This helper can provide
    both singular and plural forms of translation. It also supports interpolation.
    The helper uses a Translator instance under the hood. The Translator class
    now has a translatePlural method.
    
    J=SPR-463
    TEST=auto, manual
    
    Added new unit tests for Translator.translatePlural. Created a French Jambo
    site locally. Added a custom partial that invoked the translateWithPlural
    helper. Verified that singular and plural forms were translated proplery. Also
    verified that interpolation and translation fallbacks worked correctly with
    the new helper.

--- tests/i18n/translator/translator.js
+++ tests/i18n/translator/translator.js
@@ -3,22 +3,22 @@ const path = require('path');
 const LocalFileParser = require('../../../src/i18n/translationfetchers/localfileparser');
 const Translator = require('../../../src/i18n/translator/translator');
 
-describe('Translator works correctly', () => {
-  let translator;
-  beforeAll(async () => {
-    const translationsPath = path.join(__dirname, '../../fixtures/translations');
-    const localFileParser = new LocalFileParser(translationsPath);
+let translator;
+beforeAll(async () => {
+  const translationsPath = path.join(__dirname, '../../fixtures/translations');
+  const localFileParser = new LocalFileParser(translationsPath);
 
-    const frFRTranslations = await localFileParser.fetch('fr-FR');
-    const frTranslations = await localFileParser.fetch('fr');
-    const translations = {
-      'fr': { translation: frTranslations },
-      'fr-FR': { translation: frFRTranslations }
-    }
+  const frFRTranslations = await localFileParser.fetch('fr-FR');
+  const frTranslations = await localFileParser.fetch('fr');
+  const translations = {
+    'fr': { translation: frTranslations },
+    'fr-FR': { translation: frFRTranslations }
+  }
 
-    translator = await Translator.create('fr-FR', ['fr'], translations);
-  })
+  translator = await Translator.create('fr-FR', ['fr'], translations);
+});
 
+describe('Translations without pluralization or context', () => {
   it('simple translation works as expected', () => {
     const translation = translator.translate('Item');
     expect(translation).toEqual('Article');
@@ -33,5 +33,38 @@ describe('Translator works correctly', () => {
   it('translation fallback works as expected', () => {
     const translation = translator.translate('Breakfast');
     expect(translation).toEqual('Petit Déjeuner');
+  });
+});
+
+describe('Translations with pluralization and no context', () => {
+  it('simple pluralization works as expected', () => {
+    const translation = translator.translatePlural('Item', 'Items', 2);
+    expect(translation).toEqual('Articles');
+  });
+
+  it('pluralization and interpolation works as expected', () => {
+    const translation = translator.translatePlural(
+      'There is {{count}} item {{name}}', 
+      'There are {{count}} items {{name}}', 
+      2, 
+      { name: 'Tom' });
+    expect(translation).toEqual('Il y a 2 articles Tom')
+  });
+
+  it('falls back correctly when count is unity', () => {
+    const translation = translator.translatePlural(
+      'Missing {{count}} translation', 
+      'Missing {{count}} translations', 
+      1);
+    expect(translation).toEqual('Missing 1 translation');
+  });
+
+  it('falls back correctly when count is not unity', () => {
+    const translation = translator.translatePlural(
+      'Missing {{count}} translation {{name}}', 
+      'Missing {{count}} translations {{name}}', 
+      2,
+      { name: 'Tom' });
+    expect(translation).toEqual('Missing 2 translations Tom');
   });
 });
