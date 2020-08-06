8d47844474a9118f51c0527a353f8c255d501e9b
Connor Anderson <canderson@yext.com>
2020-08-06 18:27:13 -0400

    Add translateWithContext

--- src/commands/build/sitesgenerator.js
+++ src/commands/build/sitesgenerator.js
@@ -291,6 +291,18 @@ exports.SitesGenerator = class {
         return translator.translatePlural(singularPhrase, pluralPhrase, count, interpValues);
       }
     );
+
+    /**
+     * Translates the provided phrase depending on the context.
+     * Supports interpolation.
+     */
+    hbs.registerHelper(
+      'translateWithContext',
+      function (phrase, context, options) {
+        const interpValues = options.hash;
+        return translator.transalteWithContext(phrase, context, interpValues);
+      }
+    )
   }
 
   _registerHelpers() {
