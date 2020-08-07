9edd75770326077915307c4f27eba13ef58b661e
Alexis Grow <agrow@yext.com>
2020-08-07 15:59:53 -0400

    2 spaces instead of 4 spaces indentation in new file

--- src/models/globalconfig.js
+++ src/models/globalconfig.js
@@ -5,48 +5,48 @@ module.exports = class GlobalConfig {
 	/**
 	 * @param {Object} rawConfig
 	 */
-	constructor(rawConfig) {
+  constructor(rawConfig) {
 		/**
 		 * @type {Object}
 		 */
-		this.rawConfig = rawConfig || {};
-	}
+    this.rawConfig = rawConfig || {};
+  }
 
 	/**
 	 * Returns the locale
 	 *
 	 * @returns {String}
 	 */
-	getLocale() {
-		return this.rawConfig.locale;
-	}
+  getLocale() {
+    return this.rawConfig.locale;
+  }
 
 	/**
 	 * Returns the experienceKey
 	 *
 	 * @returns {String}
 	 */
-	getExperienceKey() {
-		return this.rawConfig.experienceKey;
-	}
+  getExperienceKey() {
+    return this.rawConfig.experienceKey;
+  }
 
 	/**
 	 * Returns the apiKey
 	 *
 	 * @returns {String}
 	 */
-	getApiKey() {
-		return this.rawConfig.apiKey;
-	}
+  getApiKey() {
+    return this.rawConfig.apiKey;
+  }
 
 	/**
 	 * Returns the raw config
 	 *
 	 * @returns {Object}
 	 */
-	getConfig() {
-		return this.rawConfig;
-	}
+  getConfig() {
+    return this.rawConfig;
+  }
 
 	/**
 	 * Creates a new {GlobalConfig} object from the given
@@ -55,7 +55,7 @@ module.exports = class GlobalConfig {
 	 * @param {GlobalConfig}
 	 * @returns {GlobalConfig}
 	 */
-	static from (globalConfig) {
-		return new GlobalConfig(globalConfig.getConfig());
-	}
+  static from(globalConfig) {
+    return new GlobalConfig(globalConfig.getConfig());
+  }
 }
