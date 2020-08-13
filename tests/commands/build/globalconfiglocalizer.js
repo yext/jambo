ee4cd9948a3ee31655e887309af93350e338a460
Alexis Grow <agrow@yext.com>
2020-08-13 17:09:35 -0400

    consolidate tests

--- tests/commands/build/globalconfiglocalizer.js
+++ tests/commands/build/globalconfiglocalizer.js
@@ -23,62 +23,27 @@ describe('GlobalConfigLocalizer generates expected localized global configs', ()
     apiKey: 'apiKeyFromGlobalConfig',
     experienceKey: 'experienceKeyFromGlobalConfig'
   });
-
   const localizationConfig = new LocalizationConfig({
     localeConfig: {
       en: {
         apiKey: 'en_apiKeyFromLocalizationConfig',
         experienceKey: 'en_experienceKeyFromLocalizationConfig'
       },
-      es: {
-        apiKey: 'es_apiKeyFromLocalizationConfig',
-      },
-      de: {
-        experienceKey: 'de_experienceKeyFromLocalizationConfig'
-      },
       fr: {}
     }
   });
 
-  it('uses localized values if provided in localization config', () => {
-    const localeToGlobalConfig = new GlobalConfigLocalizer(localizationConfig)
-      .localize(globalConfig, 'en');
+  it('merges global config and localization config correctly', () => {
+    const globalConfigLocalizer = new GlobalConfigLocalizer(localizationConfig);
 
-    expect(localeToGlobalConfig).toEqual(
+    expect(globalConfigLocalizer.localize(globalConfig, 'en')).toEqual(
       new GlobalConfig({
         locale: 'en',
         apiKey: 'en_apiKeyFromLocalizationConfig',
         experienceKey: 'en_experienceKeyFromLocalizationConfig'
       }));
-  });
 
-  it('merges global config and localization config', () => {
-    const localeToGlobalConfig = new GlobalConfigLocalizer(localizationConfig)
-      .localize(globalConfig, 'es');
-
-    expect(localeToGlobalConfig).toEqual(
-      new GlobalConfig({
-        locale: 'es',
-        apiKey: 'es_apiKeyFromLocalizationConfig',
-        experienceKey: 'experienceKeyFromGlobalConfig'
-      }));
-
-    const localeToGlobalConfig2 = new GlobalConfigLocalizer(localizationConfig)
-      .localize(globalConfig, 'de');
-
-    expect(localeToGlobalConfig2).toEqual(
-      new GlobalConfig({
-        locale: 'de',
-        apiKey: 'apiKeyFromGlobalConfig',
-        experienceKey: 'de_experienceKeyFromLocalizationConfig'
-      }));
-  });
-
-  it('uses values from global config if absent in localization config', () => {
-    const localeToGlobalConfig = new GlobalConfigLocalizer(localizationConfig)
-      .localize(globalConfig, 'fr');
-
-    expect(localeToGlobalConfig).toEqual(
+    expect(globalConfigLocalizer.localize(globalConfig, 'fr')).toEqual(
       new GlobalConfig({
         locale: 'fr',
         apiKey: 'apiKeyFromGlobalConfig',
