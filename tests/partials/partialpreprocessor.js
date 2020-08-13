9a870c6457447c8604076d79cceae8aac19b3cf9
Tom Meyer <tmeyer@yext.com>
2020-08-13 15:46:24 -0400

    Correct formatting.

--- tests/partials/partialpreprocessor.js
+++ tests/partials/partialpreprocessor.js
@@ -6,27 +6,27 @@ const PartialPreprocessor = require('../../src/partials/partialpreprocessor');
 jest.mock('../../src/i18n/translator/translator')
 
 describe('PartialPreprocessor works correctly', () => {
-    Translator.mockImplementation(() => {
+  Translator.mockImplementation(() => {
+    return {
+      translate: () => 'Bonjour',
+      translateWithContext: () => 'Mail maintenant {{name}}',
+      translatePlural: () => {
         return {
-            translate: () => 'Bonjour',
-            translateWithContext: () => 'Mail maintenant {{name}}',
-            translatePlural: () => {
-                return {
-                    1: 'Un article {{name}}',
-                    many: 'Les articles {{name}}'
-                };
-            }
+          1: 'Un article {{name}}',
+          many: 'Les articles {{name}}'
         };
-    });
-    const translator = new Translator();
-    const partialPreprocessor = new PartialPreprocessor(translator);
+      }
+    };
+  });
+  const translator = new Translator();
+  const partialPreprocessor = new PartialPreprocessor(translator);
 
-    it('transpiles all "translate" invocations in a JS partial', () => {
-        const rawJsPartial = readFileSync(
-            path.join(__dirname, '../fixtures/partials/rawcomponent.js'), 'utf8');
-        const processedJsPartial = readFileSync(
-            path.join(__dirname, '../fixtures/partials/processedcomponent.js'), 'utf8');
-        
-        expect(partialPreprocessor.process(rawJsPartial)).toEqual(processedJsPartial);
-    });
+  it('transpiles all "translate" invocations in a JS partial', () => {
+    const rawJsPartial = readFileSync(
+      path.join(__dirname, '../fixtures/partials/rawcomponent.js'), 'utf8');
+    const processedJsPartial = readFileSync(
+      path.join(__dirname, '../fixtures/partials/processedcomponent.js'), 'utf8');
+
+    expect(partialPreprocessor.process(rawJsPartial)).toEqual(processedJsPartial);
+  });
 });
