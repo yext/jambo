1aea83a5ff57549194f982a27421805da3ab9d96
Tom Meyer <tmeyer@yext.com>
2020-08-13 15:27:01 -0400

    Add a preprocessor for all partials registered with Handlebars.
    This PR adds the PartialPreprocessor class. This class performs some processing
    on partials before Jambo registers them with Handlebars. Specifically, it looks
    to transpile all usages of the 'translate' helper from a partial. If the
    translation can be resolved at compile-time, the 'translate' call is replaced
    with the translated string. If the translation must be done at run-time, the
    call is replaced with a call to the SDK's new run-time translation methods.
    Translations must be done at run-time if they require pluralization or
    interpolation.
    
    Currently, the transpilation only works for JS partials. A subsequent PR will
    add support for normal .hbs partials as well.
    
    The Translator class will need to be updated to support run-time translations.
    Specifically, the translatePlural method will provide all singular and plural
    forms of the string. The SDK's run-time method can then choose the right form
    at run-time. The translate and translateWithContext methods will need to be
    updated as well to supply a formatter string when interpolation is required.
    The SDK's run-time method will take the formatter string and the interpolation
    options and produce the right translation.
    
    J=SLAP-557
    TEST=auto
    
    Added unit tests for the PartialPreprocessor. In the tests, I mocked the
    Translator class to have the behavior outlined above.

---
+++ src/partials/models/translateinvocation.js
@@ -0,0 +1,99 @@
+const _ = require('lodash');
+
+// An enum representing the different parameter types that can appear when the
+// 'translate' helper is invoked.
+const ParamTypes = {
+    PHRASE: 'phrase',
+    PLURAL: 'pluralForm',
+    CONTEXT: 'context',
+    COUNT: 'count',
+    OTHER: 'other'
+}
+Object.freeze(ParamTypes);
+
+/**
+ * A data model representing an invocation of Jambo's 'translate' helper.
+ */
+class TranslateInvocation {
+    constructor(providedParams) {
+        this._providedParams = providedParams;
+    }
+
+    /**
+     * Whether or not this invocation requires pluralization.
+     * 
+     * @returns {boolean}
+     */
+    isUsingPluralization() {
+        return this._providedParams.hasOwnProperty(ParamTypes.PLURAL);
+    }
+
+    /**
+     * If the translation requested by the invocation can be resolved at
+     * compile-time. This is true if no pluralizaiton or interpolation is
+     * requested.
+     * 
+     * @returns {boolean}
+     */
+    canBeTranslatedStatically() {
+        return Object.keys(this._providedParams).every(
+            param => param === ParamTypes.PHRASE || param === ParamTypes.CONTEXT);
+    }
+
+    /**
+     * Returns the phrase needing translation.
+     * 
+     * @returns {string} The phrase to be translated.
+     */
+    getPhrase() {
+        return this._providedParams[ParamTypes.PHRASE];
+    }
+
+    /**
+     * Returns any included translation context.
+     * 
+     * @returns {string} The translation context.
+     */
+    getContext() {
+        return this._providedParams[ParamTypes.CONTEXT];
+    }
+
+    /**
+     * Returns any interpolation params included with the invocation. 
+     * 
+     * @returns {Object<string, string>} The interpolation params.
+     */
+    getInterpolationParams() {
+        const interpParams = _.cloneDeep(this._providedParams);
+        const paramsToRemove = 
+            [ParamTypes.CONTEXT, ParamTypes.PHRASE, ParamTypes.PLURAL];
+        paramsToRemove.forEach(param => delete interpParams[param]);
+
+        return interpParams;
+    }
+
+    /**
+     * Creates a {@link TranslateInvocation} from a partial's call to Jambo's
+     * 'translate' helper.
+     * 
+     * @param {string} invocationString The string in the partial calling 'translate'.
+     * @returns {TranslateInvocation} The resulting {@link TranslateInvocation}.
+     */
+    static from(invocationString) {
+        const paramRegex = 
+            /([a-zA-z]+=\'[a-zA-Z\s\{\}\.]+\')|([a-zA-z]+=\d+)/g;
+            
+        const parsedParams = (invocationString.match(paramRegex) || [])
+            .reduce((params, paramString) => {
+                const paramOperands = paramString.split('=');
+                const paramName = paramOperands[0];
+                const paramValue = paramOperands[1];
+                params[paramName] = paramValue;
+                return params;
+            }, {});
+        
+        return new TranslateInvocation(parsedParams);
+    }
+}
+
+module.exports = TranslateInvocation;
