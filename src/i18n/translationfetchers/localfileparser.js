844b2526dd1725c7b76c58d3968e25bc5f0e7d31
Alexis Grow <agrow@yext.com>
2020-08-24 13:12:38 -0400

    Use translation file path if provided in locale_config (#94)

--- src/i18n/translationfetchers/localfileparser.js
+++ src/i18n/translationfetchers/localfileparser.js
@@ -9,7 +9,7 @@ const { gettextToI18next } = require('i18next-conv');
 class LocalFileParser {
   /**
    * Creates a new instance of {@link LocalFileParser}.
-   * 
+   *
    * @param {string} translationsDir The local directory containing .PO files.
    * @param {Object<string,?>} options Used to optionally configure the i18next-conv
    *                                   library.
@@ -22,13 +22,17 @@ class LocalFileParser {
   /**
    * Extracts a locale's translations from the local filesystem. If no such
    * translations exist, an empty object is returned.
-   * 
+   *
    * @param {string} locale The desired locale.
+   * @param {string} translationFilePath The path to the translation file locale within
+   *                                     the translations directory; if not present,
+   *                                     defaults to [locale].po
    * @returns {Promise<Object>} A Promise containing the parsed translations in
    *                            i18next format.
   */
-  async fetch(locale) {
-    const translationFile = path.join(this._translationsDir, `${locale}.po`);
+  async fetch(locale, translationFilePath) {
+    const fileName = translationFilePath || `${locale}.po`;
+    const translationFile = path.join(this._translationsDir, fileName);
 
     if (existsSync(translationFile)) {
       const localeTranslations =
