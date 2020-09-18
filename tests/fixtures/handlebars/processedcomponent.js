7cbbf06420ff7a9355b0afa09dabb47971a4b386
cea2aj <42848445+cea2aj@users.noreply.github.com>
2020-09-18 10:24:53 -0400

    Rename output of transpilation calls (#122)
    Rename the output of the transpilation calls to match the new names in the SDK
    
    The translateJS helper now creates calls to ANSWERS.processTranslation() rather than ANSWERS.translateJS(). The translate HBS helper creates calls to a processTranslation helper rather than translate helper in the SDK.
    
    J=SLAP-654
    TEST=manual, auto
    
    Update unit tests and confirm they pass. Build an internationalized ANSWERS site with the corresponding SDK branch and observe a properly translated site.

--- tests/fixtures/handlebars/processedcomponent.js
+++ tests/fixtures/handlebars/processedcomponent.js
@@ -17,17 +17,17 @@ class standardCardComponent extends BaseCard['standard'] {
       url: profile.website || profile.landingPageUrl, // If the card title is a clickable link, set URL here
       target: '_top', // If the title's URL should open in a new tab, etc.
       titleEventOptions: this.addDefaultEventOptions(),
-      details: ANSWERS.translateJS('{\"0\":\"Un article [[name]]\",\"1\":\"Les articles [[name]]\",\"locale\":\"fr-FR\"}', {name:profile.name,count:profile.count}, profile.count), // The text in the body of the card
-      intermixed: ANSWERS.translateJS('{\"0\":\"<a href=\\"https://www.yext.com\\">Voir notre site web [[name]]</a>\",\"1\":\"<a href=\\"https://www.yext.com\\">Voir nos sites web [[name]]</a>\",\"locale\":\"fr-FR\"}', {count:2,name:name}, 2),
+      details: ANSWERS.processTranslation('{\"0\":\"Un article [[name]]\",\"1\":\"Les articles [[name]]\",\"locale\":\"fr-FR\"}', {name:profile.name,count:profile.count}, profile.count), // The text in the body of the card
+      intermixed: ANSWERS.processTranslation('{\"0\":\"<a href=\\"https://www.yext.com\\">Voir notre site web [[name]]</a>\",\"1\":\"<a href=\\"https://www.yext.com\\">Voir nos sites web [[name]]</a>\",\"locale\":\"fr-FR\"}', {count:2,name:name}, 2),
       singleQuote: 'L\'os du chien',
-      pluralizedSingleQuote: ANSWERS.translateJS('{\"0\":\"L\'homme\",\"1\":\"Les hommes\"}', {count:myCount}, myCount),
+      pluralizedSingleQuote: ANSWERS.processTranslation('{\"0\":\"L\'homme\",\"1\":\"Les hommes\"}', {count:myCount}, myCount),
       showMoreDetails: {
         showMoreLimit: 750, // Character count limit
         showMoreText: 'Show more', // Label when toggle will show truncated text
         showLessText: 'Show less' // Label when toggle will hide truncated text
       },
       CTA1: {
-        label: ANSWERS.translateJS('Mail maintenant [[id1]]', {id1:profile.name}), // The CTA's label
+        label: ANSWERS.processTranslation('Mail maintenant [[id1]]', {id1:profile.name}), // The CTA's label
         iconName: 'chevron', // The icon to use for the CTA
         url: Formatter.generateCTAFieldTypeLink(profile.c_primaryCTA), // The URL a user will be directed to when clicking
         target: '_top', // Where the new URL will be opened
