3922a3218922f2fb4b553ab29539d8fc381504f6
Tom Meyer <tmeyer@yext.com>
2020-10-07 09:23:48 -0400

    Support for importing custom Commands and adding them to the Registry.
    This PR creates the CommandImporter class. The class imports all custom
    Commands in a Jambo repo. A custom Command can be either repo or Theme
    specific. Custom Commands from the repo itself are housed in a top-level
    'commands' directory. The Theme's custom Commands live in the 'commands' folder
    under the Theme. If the same Command is specified in both the repo and the
    Theme, Jambo prefers the one from the repo. This way, Theme Commands can be
    overriden.
    
    Internally, the CommandImporter merges both 'commands' directories. It then
    treats all top-level files in the merged directory as exporting a Command
    for Jambo. This is a convention we will make clear in documentation. This
    mechanism makes it possible for people to use 'override' to create shadows
    of Theme Commands or their helpers. As soon as all the Commands are require'd,
    the merged directory is deleted from the file system. In this way, it works
    like 'static'. Another convention is that if custom Commands require helper
    functions, those either be colocated with the Command or in the
    'commands/helpers' directory.
    
    Finally, cli.js was updated so that the imported, custom Commands are added
    to the CommandRegistry. From there, the YargsFactory makes yargs commands for
    them.
    
    J=SLAP-740
    TEST=manual
    
    Tested the following:
    - Created a custom Command for the Theme.
    - Created a custom Command for a repo.
    - Ensured that a custom Command could use helper functions or node_modules.
    - If a Command is overriden in the repo, the override is preferred by Jambo.
    - If a Command's helper(s) are overridden in the repo, those are preferred
      by Jambo.

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
 const fs = require('file-system');
+const path = require('path');
 
 const { parseJamboConfig } = require('./utils/jamboconfigutils');
 const { exitWithError } = require('./utils/errorutils');
 const CommandRegistry = require('./commands/commandregistry');
 const YargsFactory = require('./yargsfactory');
+const CommandImporter = require('./commands/commandimporter');
 
 let jamboConfig;
 
@@ -16,6 +18,12 @@ try {
 }
 
 const commandRegistry = new CommandRegistry();
+const commandImporter = new CommandImporter(
+  path.join(jamboConfig.dirs.themes, jamboConfig.defaultTheme), jamboConfig.dirs.output);
+commandImporter.import().forEach(customCommand => {
+  commandRegistry.addCommand(customCommand)
+});
+
 const yargsFactory = new YargsFactory(commandRegistry);
 const options = yargsFactory.createCLI(jamboConfig);
 options.argv;
