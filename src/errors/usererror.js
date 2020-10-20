0b7e98db2557768c6d4209b8f53c75d5b2a2b997
Connor Anderson <canderson@yext.com>
2020-10-20 16:48:22 -0400

    Improve error messaging

--- src/errors/usererror.js
+++ src/errors/usererror.js
@@ -5,15 +5,17 @@ class UserError extends Error {
   constructor(message, stack) {
     super(message);
 
+    this.name = 'UserError'
+    this.exitCode = 13;
+
     if (stack) {
       this.stack = stack;
-      this.message = message;
+      this.message = `${this.name}: ${message}`;
     } else {
       Error.captureStackTrace(this, this.constructor);
     }
 
-    this.name = 'UserError'
-    this.exitCode = 13;
+    
   }
 }
 
