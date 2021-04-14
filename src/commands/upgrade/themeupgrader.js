b3f9fb9597c5443b9f0a36ce56a08253b8f75e24
Connor Anderson <canderson@yext.com>
2021-04-14 13:05:15 -0400

    support import with a URL

--- src/commands/upgrade/themeupgrader.js
+++ src/commands/upgrade/themeupgrader.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra');
 const path = require('path');
 const simpleGit = require('simple-git/promise');
 
-const { getRepoForTheme } = require('../../utils/gitutils');
+const { ThemeManager } = require('../../utils/thememanager');
 const { CustomCommand } = require('../../utils/customcommands/command');
 const { CustomCommandExecuter } = require('../../utils/customcommands/commandexecuter');
 const { ArgumentMetadata, ArgumentType } = require('../../models/commands/argumentmetadata');
@@ -158,8 +158,13 @@ class ThemeUpgrader {
    * @param {string} branch
    */
   async _recloneTheme(themeName, themePath, branch) {
+    if (!ThemeManager.isThemeKnown(themeName)) {
+      throw new UserError(
+        `The theme ${themeName} cannot be upgraded because it is not a known theme.`
+      );
+    }
     await fs.remove(themePath);
-    const themeRepoURL = getRepoForTheme(themeName);
+    const themeRepoURL = ThemeManager.getRepoForTheme(themeName);
     const updateBranch = branch || 'master';
     await git.clone(themeRepoURL, themePath, ['--branch', updateBranch]);
   }
--- src/utils/gitutils.js
+++ src/utils/gitutils.js
@@ -1,13 +1,9 @@
-const UserError = require('../errors/usererror')
+const path = require('path');
+
 /**
- * Gets the git repo URL for the given theme name.
- * @param {string} themeName 
+ * Gets the repo name from a git repo URL
+ * @param {string} repoURL
  */
-exports.getRepoForTheme = function(themeName) {
-  switch (themeName) {
-    case 'answers-hitchhiker-theme':
-      return 'https://github.com/yext/answers-hitchhiker-theme.git';
-    default:
-      throw new UserError('Unrecognized theme');
-  }
+exports.getRepoNameFromURL = function(repoURL) {
+  return path.basename(repoURL, '.git');
 }
