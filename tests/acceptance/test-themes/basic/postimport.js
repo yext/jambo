e0912835400e43633049e87977e77346ae7ca3db
Oliver Shi <oshi@yext.com>
2021-04-29 13:51:32 -0400

    dont require jambo.json

--- tests/acceptance/test-themes/basic/postimport.js
+++ tests/acceptance/test-themes/basic/postimport.js
@@ -1,22 +1,21 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const fsExtra = require('fs-extra');
-const jamboConfig = require('../../jambo.json');
 const { assign, stringify } = require('comment-json');
 
 /**
  * Updates the defaultTheme field in the jambo.json.
  * 
- * @param {Object} jamboConfig 
  * @param {string} themeName 
  */
-function updateDefaultTheme(jamboConfig, themeName) {
+function updateDefaultTheme(themeName) {
+  const jamboConfig = JSON.parse(fs.readFileSync('jambo.json', 'utf-8'));
   if (jamboConfig.defaultTheme !== themeName) {
     const updatedConfig = assign({ defaultTheme: themeName }, jamboConfig);
     fs.writeFileSync('jambo.json', stringify(updatedConfig, null, 2));
   }
 }
 
-updateDefaultTheme(jamboConfig, 'basic');
+updateDefaultTheme('basic');
 fs.writeFileSync('config/global_config.json', '{}');
 fsExtra.copySync('themes/basic/static', 'static');
