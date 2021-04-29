d4897c0fab2ca58dd9c776279d1c095165fd2bdf
Oliver Shi <oshi@yext.com>
2021-04-29 11:31:54 -0400

    add test theme setup and cleanup to this PR

---
+++ tests/acceptance/setup/playground.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const fs = require('fs');
+const fsExtra = require('fs-extra');
+const { chdir, cwd } = require('process');
+const { runCommand } = require('./utils');
+
+/**
+ * Transform all theme folders under test-themes/ into git repos.
+ */
+function setupTestThemes() {
+  function initGitRepo(dir) {
+    const originalDir = cwd();
+    chdir(dir);
+    runCommand('git init');
+    runCommand('git add -A');
+    runCommand('git commit -m "test theme"');
+    chdir(originalDir);
+  }
+  const testThemesDir = path.resolve(__dirname, '../test-themes');
+  const testThemes = fs.readdirSync(testThemesDir);
+  testThemes.forEach(themeName => {
+    initGitRepo(path.resolve(testThemesDir, themeName));
+  });
+}
+
+/**
+ * Transform all theme folders under test-themes back into regular folders,
+ * so they can be tracked properly by jambo's git repo.
+ */
+function cleanupTestThemes() {
+  const testThemesDir = path.resolve(__dirname, '../test-themes');
+  const testThemes = fs.readdirSync(testThemesDir);
+  testThemes.forEach(themeName =>{
+    const themeGitFolder = path.resolve(testThemesDir, themeName, '.git');
+    fsExtra.removeSync(themeGitFolder);
+  });
+}
