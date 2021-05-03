0e619caf11aa035062c2b3bbba9b52a119e23807
Oliver Shi <oshi@yext.com>
2021-05-03 16:23:32 -0400

    remove TODO to use yarts@17, 17 came out yesterday!

--- tests/acceptance/setup/TestInstance.js
+++ tests/acceptance/setup/TestInstance.js
@@ -4,8 +4,6 @@ const { parse } = require('shell-quote');
 /**
  * TestInstance gives Jambo acceptance tests different ways
  * to interact with the testing playground.
- *
- * TODO: use yargs.parseAsync instead of yargs.parse when it comes out in yargs@17
  */
 module.exports = class TestInstance {
   async jambo(command) {
