85a8ce9b1df05994387df0fa4ce125876e322746
Oliver Shi <oshi@yext.com>
2021-05-03 16:47:24 -0400

    update test desc

--- tests/acceptance/suites/basic-flow.js
+++ tests/acceptance/suites/basic-flow.js
@@ -6,7 +6,7 @@ const { runInPlayground } = require('../setup/playground');
 // silence jambo's noisy output
 console.log = jest.fn();
 
-it('works', () => runInPlayground(async t => {
+it('can init -> import -> create page -> build', () => runInPlayground(async t => {
   await t.jambo('init');
   await t.jambo('import --themeUrl ../test-themes/basic');
   await t.jambo('page --name index --template universal-standard');
