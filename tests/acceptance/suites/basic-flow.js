2b067ab85255cb01cc66be330b5a6d57d9a9d8df
Oliver Shi <oshi@yext.com>
2021-05-04 13:18:59 -0400

    use fixture

--- tests/acceptance/suites/basic-flow.js
+++ tests/acceptance/suites/basic-flow.js
@@ -11,17 +11,8 @@ it('can init -> import -> create page -> build', () => runInPlayground(async t =
   await t.jambo('import --themeUrl ../test-themes/basic');
   await t.jambo('page --name index --template universal-standard');
   await t.jambo('build');
-  const indexPage = fs.readFileSync('public/index.html', 'utf-8');
-  const expectedPage = 
-    `<script>
-      ANSWERS.addComponent("DirectAnswer", Object.assign({}, {
-        container: "#js-answersDirectAnswer"
-      }, {"dummyConfig":"dummy config"}));
-    </script>
-    <div
-      class="Answers-directAnswer
-      js-answersDirectAnswer"
-      id="js-answersDirectAnswer">
-    </div>`;
-  expect(indexPage).toEqualHtml(expectedPage);
+  const actualPage = fs.readFileSync('public/index.html', 'utf-8');
+  const expectedPage = fs.readFileSync(
+    '../fixtures/basic-flow/direct-answer.html', 'utf-8');
+  expect(actualPage).toEqualHtml(expectedPage);
 }));
