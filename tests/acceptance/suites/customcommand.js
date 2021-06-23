884cb468a895eaad8fbdb4bc10775f922da5f316
Christopher Jiang <cjiang@yext.com>
2021-06-23 15:39:04 -0400

    fixed spacing

--- tests/acceptance/suites/customcommand.js
+++ tests/acceptance/suites/customcommand.js
@@ -7,7 +7,8 @@ console.log = jest.fn();
 it('import and execute custom commands', () => runInPlayground(async t => {
   await t.jambo('init');
   await t.jambo('import --themeUrl ../test-themes/customcommand');
-  await t.jambo('vertical --name testvert --verticalKey testkey --template universal-standard');
+  await t.jambo(
+      'vertical --name testvert --verticalKey testkey --template universal-standard');
   const actualPage = fs.readFileSync('index.html', 'utf-8');
   const expectedPage = fs.readFileSync(
     '../fixtures/customcommand/customcommand-test.html', 'utf-8');
