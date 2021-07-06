8015eb6495f2dc9c947c7ac54de7422c3ad261b0
Oliver Shi <oshi@yext.com>
2021-07-06 12:57:22 -0400

    rebase on develop

--- tests/acceptance/suites/themeupgrader.js
+++ tests/acceptance/suites/themeupgrader.js
@@ -5,7 +5,7 @@ const ThemeManager = require('../../../src/utils/thememanager');
 const path = require('path');
 
 ThemeManager.getRepoForTheme = () => {
-    return path.resolve(__dirname, '../test-themes/basic-flow');
+  return path.resolve(__dirname, '../test-themes/basic-flow');
 };
 
 //Silence error logs
@@ -14,15 +14,15 @@ console.error = jest.fn();
 afterAll(() => console.error = error);
 
 it('tests upgrade fail', () => runInPlayground(async t => {
-    await git.cwd(process.cwd());
-    await t.jambo('init');
-    await t.jambo(
-        'import --themeUrl ../test-themes/basic-flow');
-    await git.add('.');
-    await git.commit('theme');
-    await expect(t.jambo('upgrade --branch fail')).rejects.toThrow(
-        /Remote branch fail not found in upstream origin/);
-    const diff = await git.diff();
-    expect(diff).toBe('');
-  }));
+  await git.cwd(process.cwd());
+  await t.jambo('init');
+  await t.jambo(
+    'import --themeUrl ../test-themes/basic-flow');
+  await git.add('.');
+  await git.commit('theme');
+  await expect(t.jambo('upgrade --branch fail')).rejects.toThrow(
+    /Remote branch fail not found in upstream origin/);
+  const diff = await git.diff();
+  expect(diff).toBe('');
+}));
 
