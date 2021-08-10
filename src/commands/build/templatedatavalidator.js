007588b8e6c93af045be80647cfcf80232f9f351
Oliver Shi <oshi@yext.com>
2021-08-10 18:33:15 -0400

    pass partials to templatedatavalidator
    I will cut a beta version if this is merged in,
    so that the theme's CI can work properly.
    
    J=SLAP-1489
    TEST=manual
    
    ran builds in the theme, saw that the partials can be used to validate things

--- src/commands/build/templatedatavalidator.js
+++ src/commands/build/templatedatavalidator.js
@@ -1,6 +1,5 @@
 const fs = require('file-system');
 const UserError = require('../../errors/usererror');
-const { isCustomError } = require('../../utils/errorutils');
 const { info } = require('../../utils/logger');
 
 /**
@@ -28,21 +27,24 @@ module.exports = class TemplateDataValidator {
    * @param {Object} page
    * @param {string} page.pageName name of the current page
    * @param {Object} page.pageData template arguments for the current page
+   * @param {Object<string, Function|string>} partials
+   *                          mapping of partial name to partial. Handlebars converts
+   *                          partials from strings to Functions when they are used.
    * @throws {UserError} on failure to execute hook
    * @returns {boolean} whether or not to throw an exception on bad template arguments
    */
-  validate({ pageName, pageData }) {
+  validate({ pageName, pageData, partials}) {
     if (!this._hasHook) {
-        return true;
+      return true;
     }
     try {
-        info(`Validating configuration for page "${pageName}".`);
-        const validatorFunction = require(this._templateDataValidationHook);
-        return validatorFunction(pageData);
+      info(`Validating configuration for page "${pageName}".`);
+      const validatorFunction = require(this._templateDataValidationHook);
+      return validatorFunction(pageData, partials);
     } catch (err) {
-        const msg = 
-            `Error executing validation hook from ${this._templateDataValidationHook}: `;
-        throw new UserError(msg, err.stack);
+      const msg = 
+          `Error executing validation hook from ${this._templateDataValidationHook}: `;
+      throw new UserError(msg, err.stack);
     }
   }
 }
