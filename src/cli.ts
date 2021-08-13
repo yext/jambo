c47ffa0d88796cb369ed59ad9317a9b08bcdabc0
Oliver Shi <oshi@yext.com>
2021-08-13 09:12:20 -0400

    wip

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
-const { exitWithError } = require('./utils/errorutils');
-const buildJamboCLI = require('./buildJamboCLI');
+import { exitWithError } from './utils/errorutils';
+import buildJamboCLI from './buildJamboCLI';
+import { Argv } from 'yargs';
 
 // Exit with a non-zero exit code for unhandled rejections and uncaught exceptions
 process.on('unhandledRejection', exitWithError);
 process.on('uncaughtException', exitWithError);
 
-const jambo = buildJamboCLI(process.argv);
+const jambo: Argv = buildJamboCLI(process.argv);
 jambo && jambo.parseAsync().catch(exitWithError);
