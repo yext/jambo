3d312a7ea4d2d792da6534ac82da21637e2a81ea
Oliver Shi <oshi@yext.com>
2021-08-18 15:00:11 -0400

    update canonicalizeLocale to handle 中文(chinese)
    J=SLAP-1526
    TEST=auto
    
    unit tests

--- src/utils/i18nutils.js
+++ src/utils/i18nutils.js
@@ -1,3 +1,5 @@
+const { warn } = require('npmlog');
+
 /**
  * Normalizes a locale code
  *
@@ -8,18 +10,26 @@ canonicalizeLocale = function(localeCode) {
   if (!localeCode) {
     return;
   }
-  const localeCodeSections = localeCode.replace('-', '_')
-    .split('_');
-
-  const languageIndex = 0;
-  const regionIndex = 1;
-
-  localeCodeSections[languageIndex] = localeCodeSections[languageIndex].toLowerCase();
-
-  if (localeCodeSections.length > regionIndex) {
-    localeCodeSections[regionIndex] = localeCodeSections[regionIndex].toUpperCase();
+  const localeCodeSections = localeCode.replace(/-/g, '_').split('_');
+  const numSections = localeCodeSections.length;
+  if (numSections === 1) {
+    return localeCodeSections[0].toLowerCase();
+  } else if (numSections === 2) {
+    const language = localeCodeSections[0].toLowerCase();
+    const region = localeCodeSections[1].toUpperCase();
+    return `${language}_${region}`; 
+  } else if (numSections === 3) {
+    const language = localeCodeSections[0].toLowerCase();
+    const rawModifier = localeCodeSections[1];
+    const languageModifier =
+      rawModifier.charAt(0).toUpperCase() + rawModifier.slice(1).toLowerCase();
+    const region = localeCodeSections[2].toUpperCase();
+    return `${language}-${languageModifier}_${region}`;
+  } else if (numSections > 3) {
+    warn(
+      `Encountered strangely formatted locale "${localeCode}", ` +
+      `with ${numSections} sections.`);
+    return localeCode;
   }
-
-  return localeCodeSections.join('_');
 }
 exports.canonicalizeLocale = canonicalizeLocale;
--- tests/utils/i18nutils.js
+++ tests/utils/i18nutils.js
@@ -1,15 +1,33 @@
 const { canonicalizeLocale } = require('../../src/utils/i18nutils');
 
-describe('canonicalizeLocale correctly normalizes locales', () => {
-  it('converts language to lower case and region to upper case', () => {
-    const locale = 'FR_ch';
-    const canonicalizedLocale = canonicalizeLocale(locale);
-    expect(canonicalizedLocale).toEqual('fr_CH');
-  });
-
-  it('converts dashes to underscores', () => {
-    const locale = 'fr-CH';
-    const canonicalizedLocale = canonicalizeLocale(locale);
-    expect(canonicalizedLocale).toEqual('fr_CH');
-  });
+it('converts language to lower case and region to upper case', () => {
+  const locale = 'FR_ch';
+  const canonicalizedLocale = canonicalizeLocale(locale);
+  expect(canonicalizedLocale).toEqual('fr_CH');
 });
+
+it('converts dashes to underscores', () => {
+  const locale = 'fr-CH';
+  const canonicalizedLocale = canonicalizeLocale(locale);
+  expect(canonicalizedLocale).toEqual('fr_CH');
+});
+
+describe('works for chinese', () => {
+  const testCases = [
+    ['using dashes', 'zh-Hans-CH'],
+    ['using underscores', 'zh_Hans_CH'],
+    ['underscore then dash', 'zh_Hans-CH'],
+    ['dash then underscore', 'zh-Hans_CH'],
+    ['updates casing', 'zh-hans_ch'],
+  ];
+  const expected = 'zh-Hans_CH';
+  for (const [testName, inputLocale] of testCases) {
+    runTest(testName, inputLocale, expected);
+  }
+});
+
+function runTest(testName, inputLocale, expectedLocale) {
+  it(testName, () => {
+    expect(canonicalizeLocale(inputLocale)).toEqual(expectedLocale);
+  });
+}
